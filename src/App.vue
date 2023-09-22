8cb0c00ffca63a8a56e58bdfd87cc68d11efc4f8
goldenowl281 <rathart281@gmail.com>
2023-09-22 10:26:59 +0630

    for laravel_company_test

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item mx-3">
+                <router-link class="nav-link active" aria-current="page" to="#">
+                  Company
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/employee">
+                  Employee
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.navbar-collapse {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/views/Employees/view.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
+        <h4> Employee list
+          <RouterLink to="/employee/create" class="btn btn-primary float-end">Add Employee </RouterLink>
+        </h4>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered">
+          <caption>List of employees</caption>
+          <thead style="text-align: center;">
+            <tr>
+              <th>id</th>
+              <th >Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Company</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody style="text-align: center;" v-if="this.employees.length > 0">
+            <tr v-for="(employee,index) in this.employees" :key="index">
+              <td>{{ employee.id }}</td>
+              <td>{{ employee.name}}</td>
+              <td>{{ employee.email }}</td>
+              <td>{{ employee.phone }}</td>
+              <td>{{ employee.company_name}}</td>
+              <td>
+                <RouterLink to="/edit" class="btn btn-info">Edit</RouterLink>
+                <span style="margin-left: 10px; margin-right: 10px;"></span>
+                <button type="button" class="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-else>
+            <tr>
+              <td colspan="5">Loading...</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'employees',
+  data(){
+    return {
+      employees:[]
+    }
+  },
+  mounted() {
+
+    this.getEmployees();
+  },
+  methods: {
+    getEmployees(){
+      axios.get("http://localhost:8000/api/employees")
+          .then(res => {
+            this.employees = res.data.data;
+            // console.log(this.employees)
+          })
+          .catch(error=>{
+            console.error('error fetching employees', error);
+          });
+    }
+  },
+}
+</script>
